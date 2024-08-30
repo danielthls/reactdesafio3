f325fb50fe9d93c4a3280cfa0d3a5870b0637313
Daniel Thomaz <daniel_thls@yahoo.com.br>
2024-08-30 03:53:37 -0300

    Título header com link para página inicial

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 export default function Header() {
     return (
         <header className='de3-header'>
-            <h2>Github API</h2>
+            <Link to='/'>
+                <h2>Github API</h2>
+            </Link>
         </header>
     )
 }
