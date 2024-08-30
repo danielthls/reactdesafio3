aa66dac3f3e5212f9ed6a1a667ca7f8e0b10ed7b
Daniel Thomaz <daniel_thls@yahoo.com.br>
2024-08-30 02:00:08 -0300

    Request user

--- src/routes/Main/Search/index.tsx
+++ src/routes/Main/Search/index.tsx
@@ -1,14 +1,50 @@
 
 import Button from "../../../components/Button";
+import { UserDTO } from "../../../models/User";
+import * as userService from '../../../services/UserService'
 import './styles.css'
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+type SearchData = {
+    userName: string;
+}
 
 export default function Search() {
 
-    const [userInput, setUserInput] = useState<string>('');
+    const [searchData, setSearchData] = useState<SearchData>({
+        userName: ''
+    });
 
-    const handleInputSubmit = (value: string) => {
-        setUserInput(value);
+    //const [userName, setUserName] = useState<string>('');
+
+    const [user, setUser] = useState<UserDTO>();
+    /*
+    useEffect(() => {
+        userService.findByUserName(userName)
+            .then(response => {
+                setUser(response.data)
+                console.log(response.data)
+                console.log('user: ' + user);
+            })
+
+        console.log(userName)
+    }, [userName])
+    */
+
+    function handleInputChange(event: any) {
+        const value = event.target.value;
+        const name = event.target.name
+        setSearchData({ ...searchData, [name]: value })
+    }
+
+    function handleFormSubmit(event: any) {
+        event.preventDefault();
+        userService.findByUserName(searchData.userName)
+            .then(response => {
+                setUser(response.data)
+                console.log(response.data)
+            })
+
     }
 
     return (
@@ -16,11 +52,21 @@ export default function Search() {
             <section id="de3-search-card">
                 <div className='de3-container de3-search-container'>
                     <h1>Encontre um perfil Github</h1>
-                    <form action="">
-                        <input type="text"
-                            placeholder='Usuário Github' />
+                    <form onSubmit={handleFormSubmit}>
+                        <input
+                            name="userName"
+                            value={searchData.userName}
+                            type="text"
+                            placeholder='Usuário Github'
+                            onChange={handleInputChange} />
+
                         <Button caption='Encontrar' />
                     </form>
+                </div>
+            </section>
+
+            <section id="de3-result-card">
+                <div className='de3-container de3-result-container'>
                 </div>
             </section>
 
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/system";
 
-export function findById(userName: string) {
+export function findByUserName(userName: string) {
     return axios.get(`${BASE_URL}/users/${userName}`)
 }
